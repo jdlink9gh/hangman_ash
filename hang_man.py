98551be68beea81f656cab77e39cf7b92a7e4e77
Elahi <ashiq.elahi@accenturefederal.com>
2019-05-08 15:50:47 -0400

    Added functionality like printing hangman, providing random words to choose from, blank lines

--- hang_man.py
+++ hang_man.py
@@ -1,5 +1,5 @@
 import random
-
+import string
 
 class Hangman:
 
@@ -7,7 +7,7 @@ class Hangman:
         with open("sowpods.txt") as file:
             while True:
                 new_list = []
-                max_len = input('Enter character max limit: ')
+                max_len = input('Enter max character limit: ')
                 if not max_len.isdigit():
                     print('Please enter a number')
                     continue
@@ -18,6 +18,80 @@ class Hangman:
                         if len(word) <= int_len:
                             new_list.append(word)
                             right_word = random.choice(new_list)
+                            right_list = list(right_word)
+
+                            random_string = ''.join(random.choice(string.ascii_letters) for x in range(5)) + right_word
+                            guess_string = ''.join(random.sample(random_string, len(random_string)))
+                            random_list = list(guess_string.upper())
+
+                    blank_list = ["__"] * len(right_list)
+                    print(right_list)
+                    right = 0
+                    wrong = 0
+                    hang_list = [
+                        """
+                        |‾‾‾‾‾‾‾‾|
+                        |       
+                        |      
+                        |      
+                        |
+                        """,
+                        """
+                        |‾‾‾‾‾‾‾‾|
+                        |     O
+                        |   
+                        |   
+                        |
+                        """,
+                        """
+                        |‾‾‾‾‾‾‾‾|
+                        |     O
+                        |    /
+                        |   
+                        |
+                        """,
+                        """
+                        |‾‾‾‾‾‾‾‾|
+                        |     O
+                        |    /|
+                        |   
+                        |
+                        """,
+                        """
+                        |‾‾‾‾‾‾‾‾|
+                        |     O
+                        |    /|\\
+                        |   
+                        |
+                        """,
+                        """
+                        |‾‾‾‾‾‾‾‾|
+                        |     O
+                        |    /|\\
+                        |    / 
+                        |
+                        """,
+                        """
+                        |‾‾‾‾‾‾‾‾|
+                        |     O
+                        |    /|\\
+                        |    / \\
+                        |
+                        """]
+                    while wrong != 6:
+                        print(hang_list[wrong])
+                        print('Pick from these letters: ' + ' '.join(random_list))
+                        print('Current status: ' + ' '.join(blank_list))
+                        guess_word = input('Guess a letter: ')
+                        if guess_word.upper() in right_list:
+                            right += 1
+                            print('RIGHT SELECTION')
+                        else:
+                            print('WRONG SELECTION')
+                            wrong += 1
+                    print(hang_list[6])
+                    print("Sorry you loose, the correct word was: " + right_word)
+                    break
 
 def main():
     if __name__ == "__main__":
